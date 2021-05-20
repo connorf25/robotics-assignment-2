fb23ae46ef95c6f15a1a26b44bec9f1ab0512e6f
Connor Forbes <connorforbes25@gmail.com>
2021-05-20 19:32:00 +1000

    Random maze generation complete

--- grid-world-generation.py
+++ grid-world-generation.py
@@ -2,16 +2,23 @@ import random
 from colorama import init, Fore
 # colorama needs to be initialized in order to be used
 init()
+
+unvisited = "U"
+open = "O"
+wall = "H"
+start = "S"
+goal = "G"
+
 def print_maze(maze):
     for i in range(0, len(maze)):
         for j in range(0, len(maze[0])):
-            if maze[i][j] == 'u':
+            if maze[i][j] == unvisited:
                 print(Fore.WHITE, f'{maze[i][j]}', end="")
-            elif maze[i][j] == 'O':
+            elif maze[i][j] == open:
                 print(Fore.BLUE, f'{maze[i][j]}', end="")
-            elif maze[i][j] == 'S':
+            elif maze[i][j] == start:
                 print(Fore.GREEN, f'{maze[i][j]}', end="")
-            elif maze[i][j] == 'G':
+            elif maze[i][j] == goal:
                 print(Fore.GREEN, f'{maze[i][j]}', end="")
             else:
                 print(Fore.RED, f'{maze[i][j]}', end="")
@@ -22,7 +29,7 @@ def initialize_maze(w, h):
     for i in range(0, h):
         line = []
         for j in range(0, w):
-            line.append('u')
+            line.append(unvisited)
         maze.append(line)
     return maze
 
@@ -33,10 +40,56 @@ def not_edge(x, edge):
         x -= 1
     return x
 
-open = "O"
-wall = "H"
-start = "S"
-goal = "G"
+def surrounding_cells(rand_wall, maze):
+    s_cells = 0
+    if (maze[rand_wall[0]-1][rand_wall[1]] == open):
+        s_cells += 1
+    if (maze[rand_wall[0]+1][rand_wall[1]] == open):
+        s_cells += 1
+    if (maze[rand_wall[0]][rand_wall[1]-1] == open):
+        s_cells +=1
+    if (maze[rand_wall[0]][rand_wall[1]+1] == open):
+        s_cells += 1
+    return s_cells
+
+def find_wall(rand_wall, walls):
+    for i in range(len(walls)):
+        if (walls[i][0] == rand_wall[0] and walls[i][1] == rand_wall[1]):
+            return i
+
+def handle_neighbor(direction, width, height, random_wall, maze, walls):
+    if direction == "up":
+        if (random_wall[0] != 0):
+            if (maze[random_wall[0]-1][random_wall[1]] != open):
+                maze[random_wall[0]-1][random_wall[1]] = wall
+            if ([random_wall[0]-1, random_wall[1]] not in walls):
+                walls.append([random_wall[0]-1, random_wall[1]])
+        return maze, walls
+
+    if direction == "down":
+        if (random_wall[0] != height - 1):
+            if (maze[random_wall[0]+1][random_wall[1]] != open):
+                maze[random_wall[0]+1][random_wall[1]] = wall
+            if ([random_wall[0]+1, random_wall[1]] not in walls):
+                walls.append([random_wall[0]+1, random_wall[1]])
+        return maze, walls
+        
+    if direction == "left":
+        if (random_wall[1] != 0):
+            if (maze[random_wall[0]][random_wall[1]-1] != open):
+                maze[random_wall[0]][random_wall[1]-1] = wall
+            if ([random_wall[0], random_wall[1]-1] not in walls):
+                walls.append([random_wall[0], random_wall[1]-1])
+        return maze, walls
+
+    if direction == "right":
+        if (random_wall[1] != width - 1):
+            if (maze[random_wall[0]][random_wall[1]+1] != open):
+                maze[random_wall[0]][random_wall[1]+1] = wall
+            if ([random_wall[0], random_wall[1]+1] not in walls):
+                walls.append([random_wall[0], random_wall[1]+1])
+        return maze, walls
+
 
 def generate_maze (width, height):
     walls = []
@@ -47,14 +100,108 @@ def generate_maze (width, height):
     starting_x = not_edge(starting_x, width)
     starting_y = int(random.random() * height)
     starting_y = not_edge(starting_y, height)
-    # Mark starting point
-    maze[starting_y][starting_x] = start
+    # Mark starting point as open
+    maze[starting_y][starting_x] = open
     walls.append([starting_y-1, starting_x])
     walls.append([starting_y, starting_x-1])
     walls.append([starting_y, starting_x+1])
     walls.append([starting_y+1, starting_x])
-    print_maze(maze)
+    maze[starting_y-1][starting_x] = wall
+    maze[starting_y][starting_x-1] = wall
+    maze[starting_y][starting_x+1] = wall
+    maze[starting_y+1][starting_x] = wall
+    while walls:
+        # Pick a random wall
+        random_wall = walls[int(random.random()*len(walls))-1]
 
-generate_maze(10, 10)
+        # Check if the wall is a left wall
+        if random_wall[1] != 0 and maze[random_wall[0]][random_wall[1]-1] == unvisited and maze[random_wall[0]][random_wall[1]+1] == open:
+            s_cells = surrounding_cells(random_wall, maze)
+            if s_cells < 2:
+                # Mark cell as open
+                maze[random_wall[0]][random_wall[1]] = open
+                # Handle neighbor operations
+                maze, walls = handle_neighbor("up", width, height, random_wall, maze, walls)
+                maze, walls = handle_neighbor("down", width, height, random_wall, maze, walls)
+                maze, walls = handle_neighbor("left", width, height, random_wall, maze, walls)
+            walls.remove(random_wall)
+            continue
+                    
+        # Check if wall is an upper wall
+        elif random_wall[0] != 0 and maze[random_wall[0]-1][random_wall[1]] == unvisited and maze[random_wall[0]+1][random_wall[1]] == open:
+            s_cells = surrounding_cells(random_wall, maze)
+            if s_cells < 2:
+                # Mark cell as open
+                maze[random_wall[0]][random_wall[1]] = open
+                # Handle neighbor operations
+                maze, walls = handle_neighbor("up", width, height, random_wall, maze, walls)
+                maze, walls = handle_neighbor("right", width, height, random_wall, maze, walls)
+                maze, walls = handle_neighbor("left", width, height, random_wall, maze, walls)
+            walls.remove(random_wall)
+            continue
+
+        # Check if wall is bottom wall
+        elif random_wall[0] != height-1 and maze[random_wall[0]+1][random_wall[1]] == unvisited and maze[random_wall[0]-1][random_wall[1]] == open:
+            s_cells = surrounding_cells(random_wall, maze)
+            if s_cells < 2:
+                # Mark cell as open
+                maze[random_wall[0]][random_wall[1]] = open
+                # Handle neighbor operations
+                maze, walls = handle_neighbor("down", width, height, random_wall, maze, walls)
+                maze, walls = handle_neighbor("right", width, height, random_wall, maze, walls)
+                maze, walls = handle_neighbor("left", width, height, random_wall, maze, walls)
+            walls.remove(random_wall)
+            continue
+
+        #Check if wall is a right wall
+        elif random_wall[1] != width-1 and maze[random_wall[0]][random_wall[1]+1] == unvisited and maze[random_wall[0]][random_wall[1]-1] == open:
+            s_cells = surrounding_cells(random_wall, maze)
+            if s_cells < 2:
+                # Mark cell as open
+                maze[random_wall[0]][random_wall[1]] = open
+                # Handle neighbor operations
+                maze, walls = handle_neighbor("up", width, height, random_wall, maze, walls)
+                maze, walls = handle_neighbor("right", width, height, random_wall, maze, walls)
+                maze, walls = handle_neighbor("down", width, height, random_wall, maze, walls)
+            walls.remove(random_wall)
+            continue
+        else:
+            # Delete the wall from the list anyway
+            walls.remove(random_wall)
+        continue
+
+    # Mark the remaining unvisited cells as walls
+    for i in range(height):
+        for j in range(width):
+            if (maze[i][j] == unvisited):
+                maze[i][j] = wall
+
+    # Add starting and end points
+    for i in range(height):
+        for j in range(width):
+            if (maze[i][j] == open):
+                maze[i][j] = start
+                break
+        # Ensure that nested loop is broken after start is assigned
+        else:
+            continue
+        break
+                
+    for i in reversed(range(height)):
+        for j in reversed(range(width)):
+            if (maze[i][j] == open):
+                maze[i][j] = goal
+                break
+        # Ensure that nested loop is broken after start is assigned
+        else:
+            continue
+        break
+                
+    return maze
+
+maze = generate_maze(10, 10)
+print([item for sublist in maze for item in sublist])
+print_maze(maze)
+
 
 
